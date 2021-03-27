b110d941405a4b32aadb3f8b10757ebc39ba9d3a
vbazanella <vinicius.bazanella1@gmail.com>
2021-03-27 13:38:40 -0300

    Ajusta endpoints e controllers

--- src/main/java/com/bcopstein/CtrlCorredorV1/controller/EstatisticasController.java
+++ src/main/java/com/bcopstein/CtrlCorredorV1/controller/EstatisticasController.java
@@ -1,12 +1,10 @@
 package com.bcopstein.CtrlCorredorV1.controller;
 
 import com.bcopstein.CtrlCorredorV1.dto.EstatisticasDTO;
+import com.bcopstein.CtrlCorredorV1.dto.PerformanceDTO;
 import com.bcopstein.CtrlCorredorV1.model.Evento;
 import com.bcopstein.CtrlCorredorV1.repository.EventosRepositoryImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Comparator;
 import java.util.stream.Collectors;
@@ -21,6 +19,7 @@ public class EstatisticasController {
     }
 
     @GetMapping
+    @CrossOrigin(origins = "*")
     public EstatisticasDTO estatisticas(@RequestParam final int distancia) {
         var eventos = eventosRepository.findAll()
                 .stream()
@@ -54,6 +53,40 @@ public class EstatisticasController {
         var desvioPadrao = Math.sqrt(desvPadrao / (tam - 1));
 
         var dto = new EstatisticasDTO(media, mediana, desvioPadrao, eventos.size());
+        return dto;
+    }
+
+    @GetMapping("/aumento-performance")
+    @CrossOrigin(origins = "*")
+    public PerformanceDTO aumentoPerformance(@RequestParam final int distancia,
+                                             @RequestParam final int ano) {
+
+        var eventos = eventosRepository.findAll()
+                .stream()
+                .filter(e -> e.getAno() == ano)
+                .filter(e -> e.getDistancia() == distancia)
+                .sorted((e1, e2) -> {
+                    var d1 = e1.getAno() * 365 + e1.getMes() * 30 + e1.getDia();
+                    var d2 = e2.getAno() * 365 + e2.getMes() * 30 + e2.getDia();
+                    return d1 - d2;
+                })
+                .collect(Collectors.toList());
+
+        var maiorDiff = -1;
+        var eventosDiferença = new Evento[2];
+        for (int i = 0; i < eventos.size() - 1; i ++) {
+            var atual = eventos.get(i);
+            var prox = eventos.get(i + 1);
+
+            var diff = atual.getTempoEmMinutos() - prox.getTempoEmMinutos();
+            if (diff  > maiorDiff) {
+                maiorDiff = diff;
+                eventosDiferença[0] = atual;
+                eventosDiferença[1] = prox;
+            }
+        }
+
+        var dto = new PerformanceDTO(eventosDiferença[0].getNome(), eventosDiferença[1].getNome());
         return dto;
     }
 }
--- src/main/java/com/bcopstein/CtrlCorredorV1/dto/PerformanceDTO.java
+++ src/main/java/com/bcopstein/CtrlCorredorV1/dto/PerformanceDTO.java
@@ -1,13 +1,19 @@
 package com.bcopstein.CtrlCorredorV1.dto;
 
 public class PerformanceDTO {
-    private String[] eventos;
+    private String evento1;
+    private String evento2;
 
-    public PerformanceDTO(String[] eventos) {
-        this.eventos = eventos;
+    public PerformanceDTO(String evento1, String evento2) {
+        this.evento1 = evento1;
+        this.evento2 = evento2;
     }
 
-    public String[] getEventos() {
-        return eventos;
+    public String getEvento1() {
+        return evento1;
+    }
+
+    public String getEvento2() {
+        return evento2;
     }
 }
